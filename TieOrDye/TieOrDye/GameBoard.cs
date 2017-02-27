eed45f15439bb7b61fcdaf9fd3ff6babac2ed7aa
MAIN\zxl4706 <zxl4706@g.rit.edu>
2017-02-27 15:50:12 -0500

    Some of the attributes added in to get the dimension of the game window and the user's computer resolution.

--- TieOrDye/TieOrDye/GameBoard.cs
+++ TieOrDye/TieOrDye/GameBoard.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using MonoGame.Utilities.Png;
 
 /*
 TieOrDye GameBoard Class
+Made by: Zen Lin
 */
 
 namespace TieOrDye
@@ -15,11 +23,23 @@ namespace TieOrDye
         //attributes
         bool started;
 
+        //For getting the device/computer's current resolution, each property returns an int
+        int userDisplayWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        int userDisplayHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+        //For getting the game window's resolution NEED HELP FROM THE TA
+        int userGameWindowWidth = 0;
+        int userGameWindowHeight = 0;
+        
+        //For getting the content image board's dimension
+        
+
         //Gameboard constructor
         public GameBoard()
         {
             //Game is not started by default
             started = false;
+
         }
 
         //Run to start the game
