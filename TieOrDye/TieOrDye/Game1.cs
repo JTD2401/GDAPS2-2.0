c5d50c1061ed1ad30104a39287bae32f15959423
jtd2401 <jtd2401@rit.edu>
2017-03-07 19:04:47 -0500

    Added game states, added start screen without buttons and created seperate textures the start button( will add the rest eventually), added cursor to the menu screen, created method to keep player objects in the screen, cleaned up some code.

--- TieOrDye/TieOrDye/Game1.cs
+++ TieOrDye/TieOrDye/Game1.cs
@@ -22,7 +22,14 @@ namespace TieOrDye
         Texture2D p2Tex; //P2's sprite
         Player p2; //P2's object
         Texture2D gameBoard; //GameBoard Image
-        KeyboardState currKbState; //Current keyboard state
+        //Buttons are 392 x 103
+        Texture2D inactiveStartButtonTex; //GameBoard Inactive Start Button
+        Texture2D activeStartButtonTex; //GameBoard Active Start Button
+        bool startActive; //GameBoard Start Button Active boolean
+        MouseState currMState; //Current Mouse State
+        Texture2D cursorTex; //Mouse cursor
+        Rectangle cursorRect; //Cursor rectangle
+        KeyboardState currKbState, prevKbState; //Current and previous keyboard state
 
         enum GameStates { Menu, Options, InGame, GameOver}; //Enum for game states
         GameStates currentGameState; //Attribute for current game state
@@ -48,9 +55,15 @@ namespace TieOrDye
         {
             // TODO: Add your initialization logic here
 
+            //Set initial state to menu
+            currentGameState = GameStates.Menu;
+
+            //Buttons are inactive by default
+            startActive = false;
+
             //P1 Object initialized at 0,0
             p1 = new Player(p1Tex, 0, 0, 80, 120);
-
+             
             //P2 Object initialized at 900,0
             p2 = new Player(p2Tex, 900, 0, 80, 120);
 
@@ -71,9 +84,16 @@ namespace TieOrDye
             p1Tex = Content.Load<Texture2D>("Player1");
             //Load temporary p2 image
             p2Tex = Content.Load<Texture2D>("Player2");
+            //Load mouse cursor
+            cursorTex = Content.Load<Texture2D>("Cursor");
             //Load gameboard image
-            gameBoard = Content.Load<Texture2D>("GameBoard");
-
+            gameBoard = Content.Load<Texture2D>("StartScreenNoButton");
+            //Load gameboard buttons
+            //Inactive Start button
+            inactiveStartButtonTex = Content.Load<Texture2D>("InactiveStartButton");
+            //Active Start button
+            activeStartButtonTex = Content.Load<Texture2D>("ActiveStartButton");
+            
 
             // TODO: use this.Content to load your game content here
         }
@@ -98,53 +118,102 @@ namespace TieOrDye
                 Exit();
 
             // TODO: Add your update logic here
-            
+            //Current mouse state
+            currMState = Mouse.GetState();
+            //Previous Keyboard state object
+            prevKbState = currKbState;
             //Keyboard object
             currKbState = Keyboard.GetState();
 
-            //WASD - P1 Movement
-            //Currently changes float by 1, can replace the 1 with a movement increment variable
-            //P1 Up
-            if (currKbState.IsKeyDown(Keys.W))
+            //TEMPORARY GAME STATE SET UNTIL MENU IS WORKING
+            //currentGameState = GameStates.InGame;
+
+            //Menu State
+            if(currentGameState == GameStates.Menu)
             {
-                p1.Y--; //simplified some of this code which you don't even need to do this vector has a set x and y property which you could use to change the Vector
+                //Place cursor at mouse location
+
+                //If mouse hovers over button of size 392x103(Default button size) at 40,40
+                if(cursorRect.Intersects(new Rectangle(40, 40, activeStartButtonTex.Width, activeStartButtonTex.Height)))
+                {
+                    //Make the button active
+                    startActive = true;
+                    //Check for clicking button
+                    if(currMState.LeftButton == ButtonState.Pressed)
+                    {
+                        //Start game
+                        currentGameState = GameStates.InGame;
+                    }
+                }
+                else  //When the mouse is no longer hovering over the button
+                {
+                    //Make the button inactive
+                    startActive = false;
+                }
             }
-            //P1 Left
-            if (currKbState.IsKeyDown(Keys.A))
+            //Options State
+            if (currentGameState == GameStates.Options)
             {
-                p1.X--;
+
             }
-            //P1 Down
-            if (currKbState.IsKeyDown(Keys.S))
+            //In Game State
+            if (currentGameState == GameStates.InGame)
             {
-                p1.Y++;
+                //WASD - P1 Movement
+                //Currently changes float by 1, can replace the 1 with a movement increment variable
+                //P1 Up
+                if (currKbState.IsKeyDown(Keys.W))
+                {
+                    p1.Y--; //simplified some of this code which you don't even need to do this vector has a set x and y property which you could use to change the Vector
+                }
+                //P1 Left
+                if (currKbState.IsKeyDown(Keys.A))
+                {
+                    p1.X--;
+                }
+                //P1 Down
+                if (currKbState.IsKeyDown(Keys.S))
+                {
+                    p1.Y++;
+                }
+                //P1 Right
+                if (currKbState.IsKeyDown(Keys.D))
+                {
+                    p1.X++;
+                }
+                //Arrow Keys - P2 Movement
+                //P2 Up
+                if (currKbState.IsKeyDown(Keys.Up))
+                {
+                    p2.Y--;
+                }
+                //P2 Left
+                if (currKbState.IsKeyDown(Keys.Left))
+                {
+                    p2.X--;
+                }
+                //P2 Down
+                if (currKbState.IsKeyDown(Keys.Down))
+                {
+                    p2.Y++;
+                }
+                //P2 Right
+                if (currKbState.IsKeyDown(Keys.Right))
+                {
+                    p2.X++;
+                }
+                //Prevent player objects from exiting screen
+                ScreenBorder(p1);
+                ScreenBorder(p2);
+                
             }
-            //P1 Right
-            if (currKbState.IsKeyDown(Keys.D))
+            //Game Over State
+            if (currentGameState == GameStates.GameOver)
             {
-                p1.X++;
+
             }
-            //Arrow Keys - P2 Movement
-            //P2 Up
-            if (currKbState.IsKeyDown(Keys.Up))
-            {
-                p2.Y--;
-            }
-            //P2 Left
-            if (currKbState.IsKeyDown(Keys.Left))
-            {
-                p2.X--;
-            }
-            //P2 Down
-            if (currKbState.IsKeyDown(Keys.Down))
-            {
-                p2.Y++;
-            }
-            //P2 Right
-            if (currKbState.IsKeyDown(Keys.Right))
-            {
-                p2.X++;
-            }
+
+            
 
             base.Update(gameTime);
         }
@@ -161,15 +230,76 @@ namespace TieOrDye
 
             //Start spritebatch
             spriteBatch.Begin();
-            //Draw p1 at their current position
-            spriteBatch.Draw(p1Tex, p1.PlayerRect, Color.White);
-            spriteBatch.Draw(p2Tex, p2.PlayerRect, Color.White);
+
+            //Menu State
+            if (currentGameState == GameStates.Menu)
+            {
+                //Draw gameboard
+                spriteBatch.Draw(gameBoard, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+                //Set cursor rectangle
+                cursorRect = new Rectangle(currMState.Position.X, currMState.Position.Y, 5, 5);
+                if (startActive == true)
+                {
+                    //Draw active start button at (40, 40) of size 392x103(Sprite's Default Size)
+                    spriteBatch.Draw(activeStartButtonTex, new Rectangle(40, 40, activeStartButtonTex.Width, activeStartButtonTex.Height), Color.White);
+                }
+                else if (startActive == false)
+                {
+                    //Draw inactive start button at (40, 40) of size 392x103(Sprite's Default Size)
+                    spriteBatch.Draw(inactiveStartButtonTex, new Rectangle(40, 40, activeStartButtonTex.Width, activeStartButtonTex.Height), Color.White);
+                }
+                //Draw cursor
+                spriteBatch.Draw(cursorTex, cursorRect, Color.White);
+            }
+            //Options State
+            if (currentGameState == GameStates.Options)
+            {
+
+            }
+            //InGame State
+            if (currentGameState == GameStates.InGame)
+            {
+                //Draw p1 at their current position
+                spriteBatch.Draw(p1Tex, p1.PlayerRect, Color.White);
+                spriteBatch.Draw(p2Tex, p2.PlayerRect, Color.White);
+            }
+            //Game Over State
+            if (currentGameState == GameStates.GameOver)
+            {
+
+            }
+            
+            //End spritebatch
             spriteBatch.End();
 
             base.Draw(gameTime);
         }
-
         
+        //Screen wrapping method for the player objects
+        void ScreenBorder(Player pl)
+        {
+            //Keep the player on the screen at all times
+            //If the player is all the way to the left of the screen, prevent it from moving too far to the left
+            if (pl.X < 0)
+            {
+                pl.X = 0;
+            }
+            //If the player is all the way to the right of the screen, prevent it from moving too far to the right
+            if (pl.X > GraphicsDevice.Viewport.Width - p1.PlayerRect.Width)
+            {
+                pl.X = GraphicsDevice.Viewport.Width - p1.PlayerRect.Width;
+            }
+            //If the player is all the way below the screen, prevent it from moving too far down
+            if (pl.Y > GraphicsDevice.Viewport.Height - p1.PlayerRect.Height)
+            {
+                pl.X = GraphicsDevice.Viewport.Height - p1.PlayerRect.Height;
+            }
+            //If the player is all the way above the screen, prevent it from moving too far up
+            if (pl.PlayerRect.Y < 0)
+            {
+                pl.Y = 0;
+            }
+        }
 
     }
 }
